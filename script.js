ca3d9bcf1506d9ee5ba86029f8f830a7a3ee266f
elweb-space <elweb168@gmail.com>
2026-06-14 00:24:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ Object.keys(playlists).forEach(key => {
     const hangar = document.createElement("div");
 
     hangar.className = "hangar";
-
+    
+    hangar.dataset.playlist = key;
+    
     hangar.innerHTML = `
         <h3>${playlist.title}</h3>
         <p>${playlist.description}</p>
@@ -24,7 +26,9 @@ Object.keys(playlists).forEach(key => {
     playlist.songs.forEach(song => {
 
         const card = document.createElement("div");
-
+        
+        card.id = `${key}-${song.title.replace(/\s+/g, "-")}`;
+        
         card.className = "songCard";
 
         card.innerHTML = `
@@ -137,7 +141,26 @@ missionButton.onclick=()=>{
 
         });
 
-        const song=allSongs[Math.floor(Math.random()*allSongs.length)];
+        const playlistKeys = Object.keys(playlists);
+
+        let chosenPlaylist = null;
+        let chosenSong = null;
+        
+        while (!chosenSong) {
+        
+            const key = playlistKeys[Math.floor(Math.random() * playlistKeys.length)];
+        
+            const playlist = playlists[key];
+        
+            if (playlist.songs.length > 0) {
+        
+                chosenPlaylist = key;
+        
+                chosenSong = playlist.songs[Math.floor(Math.random() * playlist.songs.length)];
+        
+            }
+        
+        }
 
         result.innerHTML=`
 
@@ -160,6 +183,45 @@ missionButton.onclick=()=>{
         </button>
 
         `;
+
+            setTimeout(() => {
+
+            // Open the correct hangar
+            document.querySelectorAll(".hangar").forEach(h => {
+    
+                const container = h.querySelector(".songContainer");
+    
+                if (h.dataset.playlist === chosenPlaylist) {
+    
+                    h.classList.add("open");
+                    container.style.display = "block";
+    
+                } else {
+    
+                    h.classList.remove("open");
+                    container.style.display = "none";
+    
+                }
+    
+            });
+    
+            // Scroll to the selected song
+            const card = document.getElementById(
+                `${chosenPlaylist}-${chosenSong.title.replace(/\s+/g, "-")}`
+            );
+    
+            card.scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+    
+            card.classList.add("selectedSong");
+    
+            setTimeout(() => {
+                card.classList.remove("selectedSong");
+            }, 3000);
+    
+        }, 800);
 
     });
 
