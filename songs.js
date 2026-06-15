aa5c3e54e625a141e3c4d74556ed1c4c5e65b463
elweb-space <elweb168@gmail.com>
2026-06-16 00:41:38 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -133,8 +133,13 @@ link:"https://open.spotify.com/track/1kFNFsAZ4iZy4vjBEtT12I?si=31ce75df3a0a490a"
 },
 
 {
-title:"Illuminati",
-link:"https://open.spotify.com/track/1kFNFsAZ4iZy4vjBEtT12I?si=31ce75df3a0a490a"
+title:"Pehli Baar",
+link:"https://open.spotify.com/track/39GcR1iJpR2RX629O0zuW5?si=1a78787aedc04cc1"
+},
+
+{
+title:"Despacito",
+link:"https://open.spotify.com/track/6habFhsOp2NvshLv26DqMb?si=fc9f784fea244150"
 }
 
 ]
@@ -150,9 +155,18 @@ description:"For some sportive and workout energy.",
 songs:[
 
 {
-title:"Run Faster",
-artist:"Artist",
-link:"#"
+title:"Zinda",
+link:"https://open.spotify.com/track/7vZz8oJ5qAqB9MghufRK5k?si=b23991435ba14915"
+},
+
+{
+title:"Unstoppable",
+link:"https://open.spotify.com/track/2J2Z1SkXYghSajLibnQHOa?si=6f547b9520a0498a"
+},
+
+{
+title:"Wake Me Up",
+link:"https://open.spotify.com/track/0nrRP2bk19rLc0orkWPQk2?si=32ecb3a147e249cc"
 }
 
 ]
@@ -168,9 +182,18 @@ description:"Songs for the traveller in you (even if it's a cab ride :p)",
 songs:[
 
 {
-title:"Run Faster",
-artist:"Artist",
-link:"#"
+title:"Ilahi",
+link:"https://open.spotify.com/track/5cgKosPPj5Cs9a2JQufUc1?si=530e02d88cb14b7d"
+},
+
+{
+title:"Viva La Vida",
+link:"https://open.spotify.com/track/1mea3bSkSGXuIRvnydlB5b?si=3064ee42e34f4662"
+},
+
+{
+title:"Yun Hi Chala Chal",
+link:"https://open.spotify.com/track/3GrTI5vAoDD3VE24VTBJAc?si=c5e73bc8ca67475e"
 }
 
 ]
@@ -186,9 +209,18 @@ description:"Faith and grace above all",
 songs:[
 
 {
-title:"Run Faster",
-artist:"Artist",
-link:"#"
+title:"O Mariyame Njan",
+link:"https://open.spotify.com/track/22KpltzWHINvAameZMBkqM?si=e2156c8ddfd74c2a"
+},
+
+{
+title:"Sacred Hymn Mashup",
+link:"https://open.spotify.com/track/05hFaS7QAqYpDvAG4rrUyU?si=62634625d67f47d1"
+},
+
+{
+title:"Our Father",
+link:"https://open.spotify.com/track/5lm24WtBNmHG4OBEVV4IGb?si=656f118b19cf405b"
 }
 
 ]
