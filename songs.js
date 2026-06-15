f8343ebb24bcd3240030ebfdf30fdcf089f606bd
elweb-space <elweb168@gmail.com>
2026-06-16 00:13:50 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -10,25 +10,22 @@ songs:[
 
 {
 title:"Ini Dooram",
-artist:"Artist",
+// artist:"Artist",
 link:"https://open.spotify.com/track/2lR0p35d2aYJYSFYE0tsfQ?si=bc6c5d453e6841e9"
 },
 
 {
 title:"A Sky Full of Stars",
-artist:"Artist",
 link:"https://open.spotify.com/track/0FDzzruyVECATHXKHFs9eJ?si=643966e142b54c7c"
 },
   
 {
 title:"Kaantha",
-artist:"Artist",
 link:"https://open.spotify.com/track/6bgQgfK4hxLqEWgSgxwKoH?si=705e951293364fd3"
 },
 
 {
 title:"Paranne",
-artist:"Artist",
 link:"https://open.spotify.com/track/6VJPFl2Jf1tnGhwaTgv04v?si=0d5f879e3a624111"
 }
   
@@ -46,11 +43,24 @@ description:"Feeling romantic?",
 songs:[
 
 {
-title:"Love Song",
-artist:"Artist",
-link:"#"
+title:"Jeevamshamayi",
+link:"https://open.spotify.com/track/59oAJRA0zzLzl9AXa1R6zh?si=db986c5d2d694d85"
+},
+  
+{
+title:"Tere Bina",
+link:"https://open.spotify.com/track/4FeczSomVWVyU4FW7xDeAI?si=bf825be314074860"
+},
+  
+{
+title:"Chand Sifarish",
+link:"https://open.spotify.com/track/5EYZZvmNAH5VZCwuzYJqoA?si=6a51e8a833e747c3"
+},
+  
+{
+title:"Perfect",
+link:"https://open.spotify.com/track/0tgVpDi06FyKpA1z0VMD4v?si=d00a40b967274750"
 }
-
 ]
 
 },
@@ -64,11 +74,20 @@ description:"Relax and drift away.",
 songs:[
 
 {
-title:"Dream",
-artist:"Artist",
-link:"#"
-}
+title:"Photograph",
+link:"https://open.spotify.com/track/1HNkqx9Ahdgi1Ixy2xkKkL?si=a46dbdc23aa148aa"
+},
 
+{
+title:"Chirapunji",
+link:"https://open.spotify.com/track/3NncVbpKJ91HPpI5TUIugV?si=a6eba1ca900045be"
+},
+
+{
+title:"Zara Zara",
+link:"https://open.spotify.com/track/7wrYBASu0OoxoDErd4Edxd?si=e5f952a8368540b6"
+}
+  
 ]
 
 },
@@ -82,9 +101,18 @@ description:"For when it's time to stay focused.",
 songs:[
 
 {
-title:"Focus",
-artist:"Artist",
-link:"#"
+title:"Bittersweet Symphony",
+link:"https://open.spotify.com/track/5nnf6869xT9hof20RQ7E15?si=5853d592b8f94fef"
+},
+
+{
+title:"Can you hear the music",
+link:"https://open.spotify.com/track/4VnDmjYCZkyeqeb0NIKqdA?si=55db7c9f7629486a"
+},
+
+{
+title:"Pirates of the Carribean",
+link:"https://open.spotify.com/track/3q309qYANeXY2OvmoqqGPm?si=86b8e405608e4905"
 }
 
 ]
@@ -100,9 +128,13 @@ description:"Move your feet!",
 songs:[
 
 {
-title:"Dance Song",
-artist:"Artist",
-link:"#"
+title:"Illuminati",
+link:"https://open.spotify.com/track/1kFNFsAZ4iZy4vjBEtT12I?si=31ce75df3a0a490a"
+},
+
+{
+title:"Illuminati",
+link:"https://open.spotify.com/track/1kFNFsAZ4iZy4vjBEtT12I?si=31ce75df3a0a490a"
 }
 
 ]
